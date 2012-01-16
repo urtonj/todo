a0f2c7e819f678988b117e4cf042acf9d96425b4
Jason Urton <jason.urton@gmail.com>
2012-01-15 23:40:51 -0500

    mobile format support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :mobile_device?
+
+  private
+
+  def mobile_device?
+  	request.format = :mobile if request.user_agent =~ /iPhone/
+  end
+
+  helper_method :mobile_device?
+
+  # def prepare_for_mobile
+  # 	puts params
+  # 	session[:mobile_param] = params[:mobile] if params[:mobile]
+  # 	if mobile_device?
+  # 		puts "mobile!"
+  # 	end
+  # 	request.format = :mobile if mobile_device?
+  # end
+
 end
