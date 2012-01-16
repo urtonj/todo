0aa2be91b3ee51063eab29f49581d0356e972799
Jason Urton <jason.urton@gmail.com>
2012-01-15 20:59:16 -0500

    order tasks

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
     def home
-        @tasks = Task.all()#:order => "position")#, :conditions => ["completed_at >  => Date.yesterday})#{
+        @tasks = Task.all(:order => "position")#, :conditions => ["completed_at >  => Date.yesterday})#{
         render "home"
     end
 end
