088b241aef814aef36444779130c34c030486c22
Jason Urton <jason.urton@gmail.com>
2012-01-15 23:53:57 -0500

    minor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :mobile_device?
+  before_filter :prepare_for_mobile
 
   private
 
   def mobile_device?
-  	request.format = :mobile if request.user_agent =~ /iPhone/
+  	# request.format = :mobile if request.user_agent =~ /iPhone/
+	  if session[:mobile_param]
+	    session[:mobile_param] == "1"
+	  else
+	    request.user_agent =~ /Mobile|webOS/
+	  end  	
   end
 
   helper_method :mobile_device?
 
-  # def prepare_for_mobile
-  # 	puts params
-  # 	session[:mobile_param] = params[:mobile] if params[:mobile]
-  # 	if mobile_device?
-  # 		puts "mobile!"
-  # 	end
-  # 	request.format = :mobile if mobile_device?
-  # end
+  def prepare_for_mobile
+  	session[:mobile_param] = params[:mobile] if params[:mobile]
+  	request.format = :mobile if mobile_device?
+  end
 
 end
