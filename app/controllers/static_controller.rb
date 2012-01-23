7e77cbe13e6f60c6b138883c500aed79f93bd72a
Jason Urton <jason.urton@gmail.com>
2012-01-22 21:26:28 -0500

    fix for pg dates?

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
     def home
-        @tasks = Task.where("completed_at > #{Date.yesterday.to_formatted_s(:db)} or completed_at is null").order("position")
+    	#{Date.yesterday.to_formatted_s(:db)}
+        @tasks = Task.where("to_timestamp(completed_at) > #{Date.yesterday} or completed_at is null").order("position")
         render "home"
     end
 end
